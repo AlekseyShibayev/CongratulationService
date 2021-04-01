eed341a7adc9e842a061e1ca9c8552aa21cbc993
fixeer <https://github.com/AlekseyShibayev/FutureSpringBootPractice.git>
2021-04-01 22:38:44 +0300

    send message to microservice

--- tempId/src/main/java/com/company/app/services/impl/NotificationServiceImpl.java
+++ tempId/src/main/java/com/company/app/services/impl/NotificationServiceImpl.java
@@ -1,11 +1,14 @@
 package com.company.app.services.impl;
 
-import com.company.app.mainLogic.TelegramBotService;
 import com.company.app.services.api.NotificationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,23 +17,24 @@ public class NotificationServiceImpl implements NotificationService {
 
     //todo stateless
     private static final boolean logEnabled = true;
-    private List<String> log;
-    @Autowired
-    private TelegramBotService bot;
+    private static final String NOTIFICATION_SERVICE_ADDRESS = "http://127.0.0.1:8081";
+
+    private final List<String> log;
 
     public NotificationServiceImpl() {
         this.log = new ArrayList<>();
     }
 
     public void eventNotification(Object message) {
-        bot.getChats().keySet().stream()
-                .map(chatId -> {
-                    SendMessage answer = new SendMessage();
-                    answer.setText(message.toString());
-                    answer.setChatId(chatId.toString());
-                    return answer;
-                })
-                .forEach(bot::sendAnswer);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject personJsonObject = new JSONObject();
+        personJsonObject.put("temp", message.toString());
+
+        HttpEntity<String> request = new HttpEntity<>(personJsonObject.toString(), headers);
+        URI locationHeader = restTemplate.postForLocation(NOTIFICATION_SERVICE_ADDRESS, request);
     }
 
     public void logNotification(Object message) {
