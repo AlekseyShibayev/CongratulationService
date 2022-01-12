28df7516f16903adf6ddaad7490a6a7450a49c2f
fixeer <https://github.com/AlekseyShibayev/FutureSpringBootPractice.git>
2022-01-12 21:14:59 +0300

    initial commit

--- src/main/java/com/company/app/Application.java
+++ src/main/java/com/company/app/Application.java
@@ -1,7 +1,5 @@
 package com.company.app;
 
-import com.company.app.mainLogic.SearchService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-	@Autowired
-	private SearchService searchService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
-		searchService.run();
+		System.out.println("Hello");
 	}
 }
